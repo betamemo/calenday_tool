c9f98d1cf0bfee5f5a5591b82c4195a87d26a038
BK <betamemo@BK.local>
2023-09-21 23:15:58 +0700

    display calendar for the the flexible layouts

--- calendar_tool.py
+++ calendar_tool.py
@@ -1,9 +1,10 @@
 from datetime import datetime, timedelta
 
 
-def get_cal_month(y, m):
+def get_cal_month(y, m, n):
     dt = datetime(y, m, 1)
     month = [[], [], [], [], [], [], []]
+    month_str = ''
 
     mon = month[0]
     mon.append('Mon:')
@@ -26,33 +27,67 @@ def get_cal_month(y, m):
     sun = month[6]
     sun.append('Sun:')
 
-    wd = dt.weekday()
-    if wd > 0:
-        for i in range(wd):
-            this_weekday = month[i]
-            this_weekday.append(' ')
-    while dt.month == m:
-        this_weekday = month[dt.weekday()]
-        this_weekday.append(str(dt.day))
-        dt = dt + timedelta(days=1)
+    m_temp = m
+    padding_temp = 8
+
+    for x in range(n):
+
+        # Add padding before day 1
+        wd = dt.weekday()
+        if wd > 0:
+            for i in range(wd):
+                this_weekday = month[i]
+                this_weekday.append(' ')
+
+        # Append month name
+        month_str = month_str + (datetime.strftime(dt, '%B')).ljust(36)
+
+        # Add day in month
+        while dt.month == m_temp:
+            this_weekday = month[dt.weekday()]
+            this_weekday.append(str(dt.day))
+            dt = dt + timedelta(days=1)
+        m_temp = dt.month
+
+        # Add padding between month
+        for i in range(len(month)):
+            if len(month[i]) < padding_temp:
+                for j in range(len(month[i]), padding_temp):
+                    month[i].append(' ')
+        padding_temp = padding_temp + 8
+
+    print(month_str)
     return month
 
 
-def get_cal_year(y):
-    for m in range(1, 13):
-        print(datetime.strftime(datetime(y, m, 1), '%B'))
-        month = get_cal_month(y, m)
-
+def get_cal_year(y, n):
+    m = 1
+    while 1 <= m <= 12:
+        month = get_cal_month(y, m, n)
         for i in range(len(month)):
             print('\t'.join(month[i]))
+        m = m + n
 
 
-year = datetime.now().year + 1
-print('Calendar ', year)
-next_year = get_cal_year(year)
-print(next_year)
+# Display calendar for the next year
+# year = datetime.now().year + 1
+# print('Calendar ', year)
+# next_year = get_cal_year(year)
+# print(next_year)
 
+# Display calendar for the chosen year
+# year = input('Enter year: ')
+# print('Calendar ', year)
+# other_year = get_cal_year(int(year))
+# print(other_year)
+
+# Display calendar for the flexible layouts
 year = input('Enter year: ')
-print('Calendar ', year)
-other_year = get_cal_year(int(year))
-print(other_year)
+month_number = input('How many months to display in a single row (1, 2, or 3)?: ')
+month_number = int(month_number)
+if month_number < 1:
+    print('Should greater than or equal 1')
+elif month_number > 3:
+    print('Should less than or equal 3')
+else:
+    get_cal_year(int(year), month_number)
